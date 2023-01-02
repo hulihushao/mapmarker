64563d02263df3b6bc87975177d4c31d64b9f361
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-01-02 16:06:47 +0800

    Update User.vue

--- src/components/User.vue
+++ src/components/User.vue
@@ -8,55 +8,82 @@
 -->
 <template>
   <div id="user">
-      <el-dropdown size="small" :hide-on-click="false" @command="handleCommand">
-      <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item :command="item.command" v-for="item in dropdownMenu":key="item.command">{{item.name}}</el-dropdown-item>
-                                          </el-dropdown-menu>
-                                          </el-dropdown>
-                                          
-            
-                </div>
+    <el-dropdown size="small" :hide-on-click="false" @command="handleCommand">
+      <el-avatar
+        style="border: 1px solid red"
+        src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+      ></el-avatar>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item :style="item.style"
+          :command="item.command"
+          v-for="item in dropdownMenu"
+          :key="item.command" :disabled="item.disabled" :divided="item.divided"
+          >{{ item.name }}</el-dropdown-item
+        >
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-    dropdownMenu:[{
-      command:"changePassword",
-      name:"修改密码" 
-    },{
-       command:"singOut",
-        name:"退出"
-    }]
+      dropdownMenu: [
+        {
+
+          command: "",
+          name: "用户名",
+          disabled:true,
+          style:{
+            color:"red",
+            'font-size':'15px'
+          },
+
+        },
+        {
+
+          command: "user",
+          name: "个人中心",
+          disabled:false,
+          divided:true,
+        },
+        {
+          command: "changePassword",
+          name: "修改密码",
+          disabled:false
+        },
+        {
+          command: "singOut",
+          name: "退出",
+          disabled:false,
+          divided:true,
+        },
+      ],
     };
   },
-  created() {
-    
-  },
+  created() {},
   methods: {
-handleCommand(command){
-  
-  switch(command){
-    case "changePassword":
-      
-      break
-    case "singOut":
-    this.singOut()
-    break
-  }},
-  singOut(){
-      this.$router.push("/login")
+    handleCommand(command) {
+      switch (command) {
+        case "changePassword":
+          break;
+        case "singOut":
+          this.singOut();
+          break;
+      }
     },
-}
+    singOut() {
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
 
 <style>
-#user{
-    position:absolute;
-    right:10px;
-    top:10px;
+#user {
+  position: absolute;
+  right: 10px;
+  top: 10px;
 }
 </style>
