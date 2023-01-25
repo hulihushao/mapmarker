fc0b51205a203d844a4fe36270de45b189896284
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-01-25 15:12:40 +0800

    Update PicActions.vue

--- src/components/PicActions.vue
+++ src/components/PicActions.vue
@@ -7,20 +7,26 @@
  * @LastEditTime: 2022-06-02 16:16:52
 -->
 <template>
-  <div id="pic-actions">
-    <i class="iconfont icon-batch"></i>
-    <div style="width: 40px; display: flex; justify-content: space-between">
-      <i
-        class="iconfont icon-liebiao1"
-        :class="{ active: isActive1 }"
-        @click="handleActive(1)"
-      ></i
-      >|
-      <i
-        class="iconfont icon-liebiaoxingshi"
-        :class="{ active: isActive2 }"
-        @click="handleActive(2)"
-      ></i>
+  <div id="pic-actions-con">
+    <div id="pic-actions">
+      <i style="" class="iconfont icon-batch" @click="handlePL"></i>
+      <div style="width: 40px; display: flex; justify-content: space-between">
+        <i
+          class="iconfont icon-liebiao1"
+          :class="{ active: isActive1 }"
+          @click="handleActive(1)"
+        ></i
+        >|
+        <i
+          class="iconfont icon-liebiaoxingshi"
+          :class="{ active: isActive2 }"
+          @click="handleActive(2)"
+        ></i>
+      </div>
+    </div>
+    <div class="plBtn" v-show="showPL">
+      <span>删除</span>
+       <span>重传</span>
     </div>
   </div>
 </template>
@@ -40,6 +46,7 @@ export default {
       isActive0: true,
       isActive1: true,
       isActive2: false,
+      showPL: false,
     };
   },
   created() {},
@@ -49,22 +56,33 @@ export default {
         this["isActive" + i] = false;
       }
       this["isActive" + num] = true;
-      this.$emit('changeActive',num)
+      this.$emit("changeActive", num);
+    },
+    handlePL() {
+      this.showPL = !this.showPL;
+      this.$store.commit("setShowChecked",this.showPL)
     },
   },
 };
 </script>
 
-<style lang="less">
-#pic-actions {
-  border-bottom: 1px solid #ccc;
-  margin-top: -30px;
+<style lang="less" >
+#pic-actions-con {
   margin-bottom: 5px;
-  display: flex;
-  justify-content: space-between;
+  border-bottom: 1px solid #ccc;
   padding: 0 10px 5px;
-  .active {
-    color: #409eff;
+  transition: all 5s;
+  #pic-actions {
+    margin-top: -30px;
+
+    display: flex;
+    justify-content: space-between;
+
+    .active {
+      color: #409eff;
+    }
+    .plplBtn {
+    }
   }
 }
 </style>
