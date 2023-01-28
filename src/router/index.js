24f55760ec02e9a08fcf9f2bf77f1579742c6b0e
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-01-28 15:13:00 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 Vue.use(VueRouter);
+
+NProgress.configure({
+  easing: "ease", // 动画方式
+  speed: 500, // 递增进度条的速度
+  showSpinner: false, // 是否显示加载icon
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3, // 初始化时的最小百分比
+});
 
 const routes = [
   {
@@ -23,6 +34,17 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // 每次切换页面时，调用进度条
+  NProgress.start();
+  next();
+});
+//当路由进入后：关闭进度条
+router.afterEach(() => {
+  // 在即将进入新的页面组件前，关闭掉进度条
+  NProgress.done();
 });
 
 export default router;
