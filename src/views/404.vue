6e4eefb7a8b4d902b94315b573ba3d1142ff9382
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-02-09 10:34:39 +0800

    Create 404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2022-06-02 14:20:28
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2022-06-02 16:16:52
+-->
+<template>
+  <div id="con-404">
+    <el-empty>
+      <el-button type="primary">返回</el-button>
+    </el-empty>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style></style>
