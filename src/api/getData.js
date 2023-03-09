7a3aabb6b2e211d438f50c2fbdf7785fad5e8bd2
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-03-09 16:05:01 +0800

    Update getData.js

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,25 +1,40 @@
+// postPic(data,option){
+//  return request('/api/map-base/add-pic','post',data, {
+                    "Content-Type": "multipart/form-data",},option)
+// },
+
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: sueRimn
  * @Date: 2022-06-01 17:16:37
  * @LastEditors: sueRimn
  * @LastEditTime: 2022-06-02 16:31:12
  */
-import request from '../utils/request/request'
+import request from "../utils/request/request";
 export default {
-  getAllPoint(params){
-   return request('/api/map-base/all-point','get',params)
+  getAllPoint(params) {
+    return request("/features", "get", params);
+    return request("/api/map-base/all-point", "get", params);
   },
-  getPic(params){
-    return request('/api/map-base/all-pic','get',params)
+  getPic(params) {
+    return request("/api/map-base/all-pic", "get", params);
   },
-  postPic(data,option){
-    return request('/api/map-base/add-pic','post',data, {
-                    "Content-Type": "multipart/form-data",},option
-)
+  postPic(data,option) {
+    return request(
+      "https://jsonplaceholder.typicode.com/posts/",
+      "post",
+      data,
+      {
+        "Content-Type": "multipart/form-data",
+      },
+      option
+    );
   },
-  postPoint(data){
-    return request('/api/map-base/add-point','post',data)
-  }
-}
+  deletePic(data) {
+    return request("/api/map-base/add-point", "post", data);
+  },
+  postPoint(data) {
+    return request("/api/map-base/add-point", "post", data);
+  },
+};
