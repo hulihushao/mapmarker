3cd0b195bfd168007ed49f46d68e68cb0776aa0b
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-03-09 16:07:55 +0800

    Update mapUtils.js

--- src/utils/map/mapUtils.js
+++ src/utils/map/mapUtils.js
@@ -11,45 +11,13 @@ import featureStyle from "@/utils/config/featureStyle";
 export default {
   async initLayer($httpRequest) {
     // 请求数据
-    // let res = await $httpRequest.getAllPoint();
+    let res = await $httpRequest.getAllPoint({});
     // console.log(res);
+    if(res.status!=200){
+      Message.error("点位加载失败!")
+    }
     // 创建图层
-    let layer = this.createLayer({
-      "timeStamp": "2022-06-01T16:44:34Z",
-      "features": [
-          {
-              "geometry": {
-                  "coordinates": [
-                      120.33,
-                      30.22
-                  ],
-                  "type": "Point"
-              },
-              "id": "MAPPOINT.3",
-              "geometry_name": "Pnt",
-              "type": "Feature",
-              "properties": {
-                  "mapPointName": "laohuyou",
-                  "modifyTime": "2022-06-02",
-                  "createTime": "2022-06-02",
-                  "comment": "旅游",
-                  "id": "3",
-                  "mpLayer": "0",
-                  "encodeStr": "c137bea4ddea45258670b84edeb1605c"
-              }
-          }
-      ],
-      "totalFeatures": "1",
-      "crs": {
-          "type": "name",
-          "properties": {
-              "name": "urn:ogc:def:crs:EPSG:4326"
-          }
-      },
-      "numberReturned": "1",
-      "type": "FeatureCollection",
-      "numberMatched": "1"
-    });
+    let layer = this.createLayer(res.data);
     layer.set("layertype", "point");
     layer.set("isBase", true);
     // 设置样式
