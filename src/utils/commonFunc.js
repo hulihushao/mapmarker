ec9bb3ca89293959dac3790472e35ab43ed35005
hulihushao <46414182+hulihushao@users.noreply.github.com>
2023-03-22 18:16:42 +0800

    Update commonFunc.js

--- src/utils/commonFunc.js
+++ src/utils/commonFunc.js
@@ -1,11 +1,12 @@
 export default {
-  deleteFeature(that, data, callback) {
+  deleteFeature(that, data, callback,errcbk) {
     that.$httpRequest
       .delFeature(data)
       .then((res) => {
         if (typeof callback == "function") callback(res);
       })
       .catch((error) => {
+        if (typeof errcbk == "function") errcbk(res);
         that.$message({
           message: "删除失败："+error.response.data.message,
           type: "error",
